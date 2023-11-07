a42cba9a0363d805f74c97c7fdad9b8b85d9cd79
Alexandre Rollin <leprogrammeur@protonmail.com>
2023-11-07 16:30:50 +0100

    Finir 3 pages traitements avant relecture

--- app/src/main/java/com/example/winlabo/Enregistrement1.java
+++ app/src/main/java/com/example/winlabo/Enregistrement1.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class EnregistrementDerogation2 extends AppCompatActivity {
+public class Enregistrement1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_enregistrement_derogation2);
+        setContentView(R.layout.activity_enregistrement_1);
     }
 }
--- app/src/main/java/com/example/winlabo/Enregistrement2.java
+++ app/src/main/java/com/example/winlabo/Enregistrement2.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class EnregistrementTraitement1 extends AppCompatActivity {
+public class Enregistrement2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_enregistrement_traitement1);
+        setContentView(R.layout.activity_enregistrement_2);
     }
 }
