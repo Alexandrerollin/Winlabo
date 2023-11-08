a65dfd21c452a696687d991bd84256558f64f1a4
Alexandre Rollin <leprogrammeur@protonmail.com>
2023-11-08 21:56:04 +0100

    Progress barre ok, continuer clique page apres declaration 6

--- app/src/main/java/com/example/winlabo/ChoixTraitementDerogation.java
+++ app/src/main/java/com/example/winlabo/ChoixTraitementDerogation.java
@@ -10,5 +10,6 @@ public class ChoixTraitementDerogation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choix_traitement_derogation);
+
     }
 }
