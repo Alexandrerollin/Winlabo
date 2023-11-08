50246201480e88fdae58d234efaeff27f0787eeb
Alexandre Rollin <leprogrammeur@protonmail.com>
2023-11-08 18:30:34 +0100

    clique jusqu'à la page déclaration 6 ok

--- app/src/main/java/com/example/winlabo/DeclarationEvenement1.java
+++ app/src/main/java/com/example/winlabo/DeclarationEvenement1.java
@@ -2,7 +2,10 @@ package com.example.winlabo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DeclarationEvenement1 extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class DeclarationEvenement1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_declaration_evenement1);
+
+        Intent previousIntent = new Intent(DeclarationEvenement1.this, Accueil.class);
+        Intent nextIntent = new Intent(DeclarationEvenement1.this, DeclarationEvenement2.class);
+
+        Button previousButton = (Button) findViewById(R.id.Precedent);
+        previousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(previousIntent);
+            }
+        });
+
+        Button nextButton = (Button) findViewById(R.id.Suivant);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(nextIntent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/winlabo/DeclarationEvenement3.java
+++ app/src/main/java/com/example/winlabo/DeclarationEvenement3.java
@@ -2,7 +2,10 @@ package com.example.winlabo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DeclarationEvenement3 extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class DeclarationEvenement3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_declaration_evenement3);
+
+        Intent previousIntent = new Intent(DeclarationEvenement3.this, DeclarationEvenement2.class);
+        Intent nextIntent = new Intent(DeclarationEvenement3.this, DeclarationEvenement4.class);
+
+        Button previousButton = (Button) findViewById(R.id.Precedent3);
+        previousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(previousIntent);
+            }
+        });
+
+        Button nextButton = (Button) findViewById(R.id.Suivant3);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(nextIntent);
+            }
+        });
     }
 }
