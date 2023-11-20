d90293bac4cc874e934147836ad4ff7d49dac97e
Alexandre Rollin <leprogrammeur@protonmail.com>
2023-11-20 16:53:46 +0100

    Ajout page Se connecter ou s'inscrire

---
+++ app/src/main/java/com/example/winlabo/SeConnecter.java
@@ -0,0 +1,14 @@
+package com.example.winlabo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeConnecter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_se_connecter);
+    }
+}
